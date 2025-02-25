6ef9da7738e30cacefe4c72ecc263d5bfbc83489
Keitoto <keitotooffice@gmail.com>
2025-02-25 19:13:07 +0100

    🎉

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,28 @@
 import type { Config } from "tailwindcss";
-
+ 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
+        system: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Open Sans",
+          "Helvetica Neue",
           "sans-serif",
           "Apple Color Emoji",
+          "Twemoji Mozilla",
           "Segoe UI Emoji",
           "Segoe UI Symbol",
           "Noto Color Emoji",
+          "EmojiOne Color",
+          "Android Emoji",
         ],
       },
     },
