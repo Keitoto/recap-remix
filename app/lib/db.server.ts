90150820f7fb36ae566aba197f973dd5e5a22bb6
Keitoto <keitotooffice@gmail.com>
2025-02-26 19:33:34 +0100

    🔥

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,26 +1,9 @@
 import type { Item, TodoActions } from '~/types';
 
-/**
- * List of todo items.
- */
 const items: Item[] = [];
 
-/**
- * Simulates an artificial delay in async operations to mimic real-world API behavior.
- * @returns {Promise<void>} A promise that resolves after the delay.
- */
-async function simulateDelay(): Promise<void> {
-  const ARTIFICIAL_DELAY = 1000;
-  await new Promise((resolve) => setTimeout(resolve, ARTIFICIAL_DELAY));
-}
-
-/**
- * An implementation of the `Todo` interface that manages a collection of todo items.
- */
 export const todos: TodoActions = {
   async create(description: string) {
-    await simulateDelay();
-
     const createdTodo: Item = {
       id: Math.random().toString(16).slice(2),
       description,
@@ -34,14 +17,10 @@ export const todos: TodoActions = {
   },
 
   async read() {
-    await simulateDelay();
-
     return items;
   },
 
   async update(id: string, fields: Partial<Omit<Item, 'id' | 'createdAt'>>) {
-    await simulateDelay();
-
     const itemIndex = items.findIndex((item) => item.id === id);
 
     if (itemIndex === -1) {
@@ -60,8 +39,6 @@ export const todos: TodoActions = {
   },
 
   async delete(id: string) {
-    await simulateDelay();
-
     const itemIndex = items.findIndex((item) => item.id === id);
 
     if (itemIndex === -1) {
@@ -74,8 +51,6 @@ export const todos: TodoActions = {
   },
 
   async clearCompleted() {
-    await simulateDelay();
-
     for (let i = items.length - 1; i >= 0; i--) {
       if (items[i].isCompleted) {
         items.splice(i, 1);
@@ -86,8 +61,6 @@ export const todos: TodoActions = {
   },
 
   async deleteAll() {
-    await simulateDelay();
-
     items.length = 0;
     return items;
   },
--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  json,
   Links,
   Meta,
   Outlet,
